0179b1e37934db441907e2b6606a5a9fa33bf229
Nafiu Amosa <naf.amosa@gmail.com>
2025-10-15 15:25:27 +0100

    first commit and old version deleted

---
+++ TodoAppV1.0/TodoAppV1.0/TodoAppV1_0App.swift
@@ -0,0 +1,32 @@
+//
+//  TodoAppV1_0App.swift
+//  TodoAppV1.0
+//
+//  Created by Nafiu Amosa on 15/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoAppV1_0App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TodoAppV1.0/TodoAppV1.0Tests/TodoAppV1_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppV1_0Tests.swift
+//  TodoAppV1.0Tests
+//
+//  Created by Nafiu Amosa on 15/10/2025.
+//
+
+import Testing
+@testable import TodoAppV1_0
+
+struct TodoAppV1_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
